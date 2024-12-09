cef1a36043614c2973d101b9ad04bfdb62b237c2
YourGitHubUsername <YourGitHubEmail>
2024-12-09 23:37:16 +0530

    ch

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter,createBrowserRouter,RouterProvider } from 'react-router-dom';
 import Daillogin from './Component/Daillogin';
 import Store from './Component/Store';
-import Experience from "./Component/Experience"
-import Achievements from './Component/Timepas';
 
 const router=createBrowserRouter([
   {
@@ -17,11 +15,8 @@ const router=createBrowserRouter([
   {
     path:"/dAilylogin", 
     element:<Daillogin/>
-  },
-  {
-    path:"/experience", 
-    element:<Achievements/>
   }
+  
 ])
  ReactDOM.createRoot(document.getElementById('root')).render(
   <Store>
